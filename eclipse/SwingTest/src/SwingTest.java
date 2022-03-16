6ab18e6ac83b620a6e2f8554b42858a6a9f28bf3
robynmyra <98797776+robynmyra@users.noreply.github.com>
2022-03-16 16:08:54 -0700

    added comment

--- eclipse/SwingTest/src/SwingTest.java
+++ eclipse/SwingTest/src/SwingTest.java
@@ -17,6 +17,8 @@ public class SwingTest {
 		f.setSize(400,500);//400 width and 500 height  
 		f.setLayout(null);//using no layout managers  
 		f.setVisible(true);//making the frame visible  
+		
+		//robyn
 	}
 
 }
